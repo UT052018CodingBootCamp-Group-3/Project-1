bd92e2938d3337a47a0de88b7b8570667c23338e
TheMelonOne <paichun.wang@yahoo.com>
2018-07-24 13:56:51 -0500

    added button for expand and shrink filter results

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,40 @@
 $(".healthMore").on("click", function () {
     event.preventDefault();
     var status = $(".healthMore").text()
-    if (status === 'See More')
-    {
+    if (status === 'See More') {
         $(".hiddenHealth").css("display", "block")
         $(".healthMore").text("See Less")
-    }else{
+    } else {
         $(".hiddenHealth").css("display", "none")
         $(".healthMore").text("See More")
-        
     }
 });
+
+$('input[type=submit]').on("click", function () {
+    event.preventDefault();
+    console.log("Search Button Clicked")
+    var output = $('input').val()
+    console.log(output)
+})
+
+var food = []
+
+function checkBox(box){
+    var pass = box.checked
+    if(pass)
+    {
+        console.log("Checked " + box.id);
+    }
+    else
+    {
+        console.log("UnChecked " + box.id);
+    }
+}
+
+// function foodOption(type)
+// {
+//     food.push(type.id)
+//     console.log(food)
+
+//     $()
+// }
